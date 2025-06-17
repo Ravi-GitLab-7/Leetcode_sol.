98169ff97316eec03794037e580c2db2bf5ab881
Ravi Raj <msd7ravis@gmail.com>
2025-06-17 11:21:19 +0530

    Time: 1 ms (72.04%), Space: 45.7 MB (63.82%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,34 +1,63 @@
 class Solution {
     public void setZeroes(int[][] arr) {
-       int m = arr.length, n = arr[0].length;
-        boolean[] row = new boolean[m];
-        boolean[] coln = new boolean[n];
+           int m = arr.length, n = arr[0].length;
+        boolean zeroRow = false, zeroColn = false;
 
-        // Mark the rows and columns where 0s are present
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (arr[i][j] == 0) {
-                    row[i] = true;
-                    coln[j] = true;
-                }
-            }
-        }
-
-        // Set the rows to 0
-        for (int i = 0; i < m; i++) {
-            if (row[i]) {
-                for (int j = 0; j < n; j++) {
-                    arr[i][j] = 0;
-                }
-            }
-        }
-
-        // Set the columns to 0
+        // check 0th row
         for (int j = 0; j < n; j++) {
-            if (coln[j]) {
-                for (int i = 0; i < m; i++) {
+            if (arr[0][j] == 0) {
+                zeroRow = true;
+                break;
+            }
+        }
+
+        // check 0th column
+        for (int i = 0; i < m; i++) {
+            if (arr[i][0] == 0) {
+                zeroColn = true;
+                break;
+            }
+        }
+
+        // Traverse submatrix without going to 0th row and 0th col
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (arr[i][j] == 0) {
+                    arr[0][j] = 0;
+                    arr[i][0] = 0;
+                }
+            }
+        }
+
+        // traverse 0th row (set entire column if arr[0][j] == 0)
+        for (int j = 1; j < n; j++) { // FIX: j < n instead of j < m
+            if (arr[0][j] == 0) {
+                for (int i = 1; i < m; i++) { // FIX: i < m instead of i < n
                     arr[i][j] = 0;
                 }
+            }
+        }
+
+        //traverse 0th column (set entire row if arr[i][0] == 0)
+        for (int i = 1; i < m; i++) { // FIX: i < m instead of i < n
+            if (arr[i][0] == 0) {
+                for (int j = 1; j < n; j++) { // FIX: j < n instead of j < m
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        // If zeroRow is true, set 0th row to 0
+        if (zeroRow == true) {
+            for (int j = 0; j < n; j++) {
+                arr[0][j] = 0;
+            }
+        }
+
+        // If zeroColn is true, set 0th column to 0
+        if (zeroColn == true) {
+            for (int i = 0; i < m; i++) {
+                arr[i][0] = 0;
             }
         }
     }
