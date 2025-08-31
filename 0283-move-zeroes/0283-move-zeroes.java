713220a1b5e5dfecb7a62f0242ecca0a3372ba4b
Ravi Raj <msd7ravis@gmail.com>
2025-08-31 18:15:30 +0530

    Time: 133 ms (5.01%), Space: 46.8 MB (6.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -7,7 +7,7 @@ class Solution {
             if(ele==0) noz++;
         }
 
-        for(int x =0;x<n-1;x++){  
+        for(int x =0;x<noz;x++){  
             for (int i = 0; i <n-1-x ; i++) {
                 if(arr[i]==0){
                     int temp = arr[i];
