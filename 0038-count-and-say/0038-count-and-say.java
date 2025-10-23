c44c5b7a10f1e868aa09b7f7163a4574994a701d
Ravi Raj <msd7ravis@gmail.com>
2025-10-23 15:58:36 +0530

    Time: 17 ms (10.79%), Space: 45.4 MB (6.8%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,22 +1,25 @@
 class Solution {
     public String countAndSay(int n) {
-        if(n==1) return "1"; // base case
-        String s = countAndSay(n-1);
-        int i=0,j=0;
+        if (n == 1)
+            return "1"; // base case
+        String s = countAndSay(n - 1)+"@";  //any thing u want to put symbol
+        //now modify
+        int i = 0, j = 0;
         String ans = "";
-        while(j<s.length()){
-            if(s.charAt(i) == s.charAt(j)) j++;
-            else{
+        while (j < s.length()) {
+            if (s.charAt(i) == s.charAt(j))
+                j++;
+            else {
                 int len = j - i;
                 ans += len;
                 ans += s.charAt(i);
                 i = j;
             }
         }
-        int len= j - i;
-        ans += len;
-        ans += s.charAt(i);
-        i = j;
+        // int len = j - i;  // if add sbmol then don't need to do this
+        // ans += len;
+        // ans += s.charAt(i);
+        // i = j;
         return ans;
     }
 }
