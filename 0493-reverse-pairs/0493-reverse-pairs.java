a653643f6a93ae42986438727a66f93c691950cd
Ravi Raj <msd7ravis@gmail.com>
2025-10-28 18:39:32 +0530

    Time: 41 ms (84.9%), Space: 56.4 MB (5.25%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,44 +1,56 @@
 class Solution {
     static int count;
-    public static void inversion(int []a,int []b){
-            int i =0,j=0;
-            while(i<a.length && j<b.length){
-                if((long)a[i]>(long)2*(long)b[j]){
-                    count +=(a.length -i);
-                    j++;
-                }
-                else i++;
-            }
+
+    public static void inversion(int[] a, int[] b) {
+        int i = 0, j = 0;
+        while (i < a.length && j < b.length) {
+            if ((long) a[i] > (long) 2 * (long) b[j]) {
+                count += (a.length - i);
+                j++;
+            } else
+                i++;
         }
-        public static void merge(int[] a, int[] b, int[] c){
-            int i = 0, j = 0, k = 0;
-            while(i < a.length && j < b.length){
-                if(a[i] < b[j]) c[k++] = a[i++];
-                else c[k++] = b[j++];
-            }
-            while(i < a.length) c[k++] = a[i++];
-            while(j < b.length) c[k++] = b[j++];
+    }
+
+    public static void merge(int[] a, int[] b, int[] c) {
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j])
+                c[k++] = a[i++];
+            else
+                c[k++] = b[j++];
         }
-        public static void mergesort(int []arr){
-            int n = arr.length;
-            // base case
-            if(n <= 1) return;
-            // create two arrays
-            int[] a = new int[n/2];
-            int[] b = new int[n - n/2];
-            // copy elements
-            for(int i = 0; i < n/2; i++) a[i] = arr[i];
-            for(int i = 0; i < n - n/2; i++) b[i] = arr[i + n/2];
-            // recursive calls
-            mergesort(a);
-            mergesort(b);
-            //InversionCount
-            inversion(a,b);
-            // final merge
-            merge(a, b, arr);
-            // for delete extra spaces
-            a = null; b = null;
-        }
+        while (i < a.length)
+            c[k++] = a[i++];
+        while (j < b.length)
+            c[k++] = b[j++];
+    }
+
+    public static void mergesort(int[] arr) {
+        int n = arr.length;
+        // base case
+        if (n <= 1)
+            return;
+        // create two arrays
+        int[] a = new int[n / 2];
+        int[] b = new int[n - n / 2];
+        // copy elements
+        for (int i = 0; i < n / 2; i++)
+            a[i] = arr[i];
+        for (int i = 0; i < n - n / 2; i++)
+            b[i] = arr[i + n / 2];
+        // recursive calls
+        mergesort(a);
+        mergesort(b);
+        //InversionCount
+        inversion(a, b);
+        // final merge
+        merge(a, b, arr);
+        // for delete extra spaces
+        a = null;
+        b = null;
+    }
+
     public int reversePairs(int[] nums) {
         count = 0;
         mergesort(nums);
