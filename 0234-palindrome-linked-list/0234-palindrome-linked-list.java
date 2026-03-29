c9164a3d1f0095be0f5cb4aa44c973ab4d243a62
Ravi Raj <ravi_249026@saitm.ac.in>
2026-03-29 15:29:52 +0530

    Time: 4 ms (69.64%), Space: 94.5 MB (50.05%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,4 +1,5 @@
 class Solution {
+
     public ListNode reverseList(ListNode head){
         ListNode curr = head;
         ListNode prev = null;
@@ -10,33 +11,31 @@ class Solution {
         }
         return prev;
     }
+
     public boolean isPalindrome(ListNode head) {
         if(head == null || head.next == null) return true;
 
-        // create copy list
-        ListNode newHead = new ListNode(head.val);
-        ListNode t1 = head.next;
-        ListNode t2 = newHead;
+        // Step 1: find middle
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while(t1 != null){
-            ListNode temp = new ListNode(t1.val);
-            t2.next = temp;
-            t2 = t2.next;
-            t1 = t1.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        // reverse copy
-        newHead = reverseList(newHead);
+        // Step 2: reverse second half
+        ListNode j = reverseList(slow);
 
-        // compare
-        t1 = head;
-        t2 = newHead;
+        // Step 3: compare both halves
+        ListNode i = head;
 
-        while(t1 != null){
-            if(t1.val != t2.val) return false;
-            t1 = t1.next;
-            t2 = t2.next;
+        while(j != null){
+            if(i.val != j.val) return false;
+            i = i.next;
+            j = j.next;
         }
+
         return true;
     }
 }
