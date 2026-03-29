2cc325c9adb4b98120b0f7368e975d3d8ce83cc0
Ravi Raj <ravi_249026@saitm.ac.in>
2026-03-29 18:56:20 +0530

    Time: 0 ms (100%), Space: 42.9 MB (88.27%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -9,39 +9,46 @@
  * }
  */
 class Solution {
-    //  public ListNode reverseList(ListNode head) {
-    //     // by iterative method
-    //     ListNode curr = head;
-    //     ListNode prev = null;
-    //     ListNode next = null;
+     public ListNode reverseList(ListNode head) {
+        // by iterative method
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next = null;
 
-    //     while(curr != null){
-    //         next = curr.next;
-    //         curr.next = prev;
-    //         prev = curr;
-    //         curr = next;
-    //     }
-    //  }
-    public ListNode reverseBetween(ListNode head, int left, int right) {
-        //create node 3
-      ListNode a= null,b= null,c=bull,d= null;
-      int poss = 1;
-      ListNode temp = head;
-      if(poss == left-1) temp =a;
-      if(poss == left) temp =b;
-      if(poss == right) temp =c;
-      if(poss == right+1) temp =d;
-    temp = temp.next;
-    poss++;
-    //now split the list
-    if(a!= null) a.next = null;
-    if(c!= null) c.next = null;
-    //reverse
-    reverseList(b);
-    //now link
-    if(a!= null) a.next = c;
-    b.next = d;
-    if(a==null) return c;
-    return head;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+     }
+       public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode a = null, b = null, c = null, d = null;
+        int poss = 1;
+        ListNode temp = head;
+        // traverse properly
+        while(temp != null){
+            if(poss == left - 1) a = temp;
+            if(poss == left) b = temp;
+            if(poss == right) c = temp;
+            if(poss == right + 1) d = temp;
+
+            temp = temp.next;
+            poss++;
+        }
+
+        // split
+        if(a != null) a.next = null;
+        if(c != null) c.next = null;
+
+        // reverse
+        ListNode newHead = reverseList(b);
+        //reconnect
+        if(a != null) a.next = newHead;
+        b.next = d;
+        //handle left = 1
+        if(a == null) return newHead;
+        return head;
     }
 }
