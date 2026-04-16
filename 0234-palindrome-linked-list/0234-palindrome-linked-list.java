279e58b6a8400d1184d95ec318bc3d2dc85af022
Ravi Raj <ravi_249026@saitm.ac.in>
2026-04-16 14:49:59 +0530

    Time: 15 ms (10.16%), Space: 101.8 MB (7.9%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,11 +1,31 @@
 class Solution {
 
-    public ListNode reverseList(ListNode head){
-     //method 1
+    public boolean isPalindrome(ListNode head) {
+
+        Stack<Integer> stack = new Stack<>();
+        ListNode temp = head;
+
+        // Push all values into stack
+        while (temp != null) {
+            stack.push(temp.val);
+            temp = temp.next;
+        }
+
+        // Compare values
+        while (head != null) {
+            if (stack.pop() != head.val) {
+                return false;
+            }
+            head = head.next;
+        }
+
+        return true;
+    }
+}
+   //method 1
     // public boolean isPalindrome(ListNode head) {
     //     if(head == null || head.next == null) return true;
-
-    //     // create copy list
+    //      create copy list
     //     ListNode newHead = new ListNode(head.val);
     //     ListNode t1 = head.next;
     //     ListNode t2 = newHead;
@@ -31,42 +51,41 @@ class Solution {
     //     }
     //     return true;
     // }
-        ListNode curr = head;
-        ListNode prev = null;
-        while(curr != null){
-           ListNode next = curr.next;
-           curr.next = prev;
-           prev = curr;
-           curr = next; 
-        }
-        return prev;
-    }
+    //     ListNode curr = head;
+    //     ListNode prev = null;
+    //     while(curr != null){
+    //        ListNode next = curr.next;
+    //        curr.next = prev;
+    //        prev = curr;
+    //        curr = next; 
+    //     }
+    //     return prev;
+    // }
 
-    public boolean isPalindrome(ListNode head) {
-        // method 2
-        if(head == null || head.next == null) return true;
+    // public boolean isPalindrome(ListNode head) {
+    //     // method 2
+    //     if(head == null || head.next == null) return true;
 
-        // Step 1: find middle
-        ListNode slow = head;
-        ListNode fast = head;
+    //     // Step 1: find middle
+    //     ListNode slow = head;
+    //     ListNode fast = head;
 
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }
+    //     while(fast != null && fast.next != null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
 
-        // Step 2: reverse second half
-        ListNode j = reverseList(slow);
+    //     // Step 2: reverse second half
+    //     ListNode j = reverseList(slow);
 
-        // Step 3: compare both halves
-        ListNode i = head;
+    //     // Step 3: compare both halves
+    //     ListNode i = head;
 
-        while(j != null){
-            if(i.val != j.val) return false;
-            i = i.next;
-            j = j.next;
-        }
+    //     while(j != null){
+    //         if(i.val != j.val) return false;
+    //         i = i.next;
+    //         j = j.next;
+    //     }
 
-        return true;
-    }
-}
+    //     return true;
+    // }
