85fba4d0215975602a5e77ec03332f18c410d583
Ravi Raj <ravi_249026@saitm.ac.in>
2026-09-08 11:28:00 +0530

    Time: 1 ms (43.77%), Space: 47.3 MB (16.49%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,18 +14,18 @@
  * }
  */
 class Solution {
-    static int maxDia;
-    public int levels(TreeNode root) {
+    // static int maxDia; if i dont have to apply globally
+    public int levels(TreeNode root, int [] maxDia) {
         if (root == null) return 0;
-        int leftLevel = levels(root.left);
-        int rightLevel = levels(root.right);
+        int leftLevel = levels(root.left,maxDia);
+        int rightLevel = levels(root.right,maxDia);
         int Dia = leftLevel + rightLevel;
-        maxDia = Math.max(Dia,maxDia);
+        maxDia[0] = Math.max(Dia,maxDia[0]);
         return 1 + Math.max(leftLevel,rightLevel);
     }
     public int diameterOfBinaryTree(TreeNode root) {
-        maxDia = 0;
-        levels(root);
-        return maxDia;
+        int [] maxDia = {0};
+        levels(root,maxDia);
+        return maxDia[0];
     }
 }
