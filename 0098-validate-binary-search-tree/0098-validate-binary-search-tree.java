b5661e731ecc184e2cd947f3f245bc99b1fcfa2d
Ravi Raj <ravi_249026@saitm.ac.in>
2026-09-16 15:14:42 +0530

    Time: 0 ms (100%), Space: 45 MB (79.69%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,33 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public long max(TreeNode root){
-        if(root==null) return Long.MIN_VALUE;
-        long a = root.val,b= max(root.left),c = max(root.right);
-        return Math.max(a,Math.max(b,c));
+
+    static boolean flag;
+    public long max(TreeNode root) {
+        if (root == null) return Long.MIN_VALUE;
+        long a = root.val;
+        long b = max(root.left);
+        long c = max(root.right);
+        if (b >= root.val) {
+            flag = false;
+        }
+        return Math.max(a, Math.max(b, c));
     }
-    public long min(TreeNode root){
-        if(root==null) return Long.MAX_VALUE;
-        long a = root.val,b= min(root.left),c = min(root.right);
-        return Math.min(a,Math.min(b,c));
+    public long min(TreeNode root) {
+        if (root == null) return Long.MAX_VALUE;
+        long a = root.val;
+        long b = min(root.left);
+        long c = min(root.right);
+        if (c <= root.val) {
+            flag = false;
+        }
+        return Math.min(a, Math.min(b, c));
     }
     public boolean isValidBST(TreeNode root) {
-        if(root== null) return true;
-        if(root.val<=max(root.left)) return false;
-        if(root.val>=min(root.right)) return false;
-        return isValidBST(root.left) && isValidBST(root.right);
+        flag = true;
+        max(root);
+        min(root);
+        return flag;
     }
 }
