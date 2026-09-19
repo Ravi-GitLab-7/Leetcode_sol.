da83ca0ffc1b71f8e74825c588299821ad427387
Ravi Raj <ravi_249026@saitm.ac.in>
2026-09-19 15:34:27 +0530

    Time: 0 ms (100%), Space: 43.8 MB (70.18%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -14,19 +14,17 @@
  * }
  */
 class Solution {
-    public void preorder(TreeNode root,List<TreeNode> pre) {
-       if(root==null) return;
-       pre.add(root);
-       preorder(root.left,pre);
-       preorder(root.right,pre);
-    }
     public void flatten(TreeNode root) {
-        List<TreeNode> pre = new ArrayList<>();
-        preorder(root,pre);
-        int n = pre.size();
-        for(int i =0;i<n;i++){
-            if(i!=n-1) pre.get(i).right = pre.get(i+1);
-            pre.get(i).left = null;
-        }
+        if(root== null) return ;
+        if(root.left == null && root.right== null) return;
+        TreeNode lst = root.left;
+        TreeNode rst = root.right;
+        flatten(lst);
+        flatten(rst);
+        root.left = null;
+        root.right = lst;
+        TreeNode temp = root;
+        while(temp.right != null) temp = temp.right;
+        temp.right = rst;
     }
 }
