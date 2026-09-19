8303cf63b1c3c98f6d2ae603a39c966c4cbaeb5d
Ravi Raj <ravi_249026@saitm.ac.in>
2026-09-19 14:13:47 +0530

    Time: 0 ms (100%), Space: 46.9 MB (18.08%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -1,37 +1,33 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int count =0;
-    int answer = 0;
     public int kthSmallest(TreeNode root, int k) {
-        inorder(root,k);
-        return answer;
-    }
-    public void inorder(TreeNode root,int k){
-        if(root==null) return;
-        // go left
-        inorder(root.left,k);
-        // add count
-        count++;
-        // if matched
-        if(count==k){
-            answer = root.val;
-            return;
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode pred = curr.left;
+                // Find inorder predecessor
+                while (pred.right != null && pred.right != curr) {
+                    pred = pred.right;
+                }
+                if (pred.right == null) { // link
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                else { // unlink
+                    pred.right = null;
+                    // Visit current
+                    k--;
+                    if (k == 0)
+                        return curr.val;
+                    curr = curr.right;
+                }
+            }
+            else { // visit current
+                k--;
+                if (k == 0)
+                    return curr.val;
+                curr = curr.right;
+            }
         }
-        // go right
-        inorder(root.right,k);
+        return -1;
     }
 }
