7f4cad5f3e7862e153be22de60b1392290c28ee6
Ravi Raj <ravi_249026@saitm.ac.in>
2026-09-25 10:07:15 +0530

    Time: 0 ms (100%), Space: 46.6 MB (32.09%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -1,83 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
 class Solution {
-
-    public Node deepCopy(Node head) {
-        if (head == null) return null;
-
-        Node head2 = new Node(head.val);
-        Node t1 = head.next;
-        Node t2 = head2;
-
-        while (t1 != null) {
-            Node temp = new Node(t1.val); // FIX: create new node
-            t2.next = temp;
-            t2 = t2.next;
-            t1 = t1.next;
-        }
-        return head2;
-    }
-
-    public void connectAlternatelly(Node head, Node head2) {
-        Node t1 = head;
-        Node t2 = head2;
-
-        Node dummy = new Node(-1);
-        Node t = dummy;
-
-        while (t1 != null && t2 != null) {
-            t.next = t1;
-            t1 = t1.next;
-            t = t.next;
-
-            t.next = t2;
-            t2 = t2.next;
-            t = t.next;
-        }
-    }
-
-    public void assignRandom(Node head, Node head2) {
-        Node t1 = head;
-
-        while (t1 != null) {
-            Node t2 = t1.next; 
-
-            if (t1.random != null) {
-                t2.random = t1.random.next;
-            }
-
-            t1 = t1.next.next;
-        }
-    }
-
-    public void split(Node head, Node head2) {
-        Node t1 = head;
-        Node t2 = head2;
-
-        while (t1 != null && t2 != null) {
-            t1.next = t2.next;
-            t1 = t1.next;
-
-            if (t1 == null) break;
-
-            t2.next = t1.next;
-            t2 = t2.next;
-        }
-    }
-
     public Node copyRandomList(Node head) {
-        if (head == null) return null;
+        if(head==null) return null;
+        Node head2 = new Node(head.val);
+        Node temp2 = head2;
+        Node temp = head.next;
 
-        // make deep copy
-        Node head2 = deepCopy(head);
+        while (temp != null) {
+            Node dup = new Node(temp.val);
+            temp2.next = dup;
+            temp2 = dup;
+            temp = temp.next;
+        }
 
-        // join head1 and head2 alternately
-        connectAlternatelly(head, head2);
+        HashMap<Node, Node> map = new HashMap<>();
+        temp2 = head2;
+        temp = head;
 
-        // assign random pointer
-        assignRandom(head, head2);
+        while (temp != null) {
+            map.put(temp, temp2);
+            temp = temp.next;
+            temp2 = temp2.next;
+        }
 
-        // split the linked-list
-        split(head, head2);
+        for (Node original : map.keySet()) {
+            Node duplicate = map.get(original);
 
-        return head2; 
+            if (original.random != null)
+                duplicate.random = map.get(original.random);
+        }
+
+        return head2;
     }
 }
